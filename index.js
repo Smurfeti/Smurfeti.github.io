8a6dbb09cae65b0a83f145cfe3f9e6b6651c8bb1
Your Name <matveynikishaev@gmail.com>
2022-11-14 13:32:17 +0300

    add age control

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const signIn = document.querySelectorAll('.text-end button')
 
-
 signIn.forEach(element => {
     element.addEventListener('click', () => {
         window.open('reg.html')
     })
 });
 
+const age = document.querySelector('.age')
 
+function show(){
+    age.classList.add('NO')
+}
+
+const close_btn = document.querySelector('.age button')
+
+close_btn.addEventListener('click', ()=> {
+    age.classList.add('NO')
+})
+
+setTimeout(show,4000)
